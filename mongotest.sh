7222e80cf12fa5717bffad8e79399ca15e7e8250
x20157576 <x20157576@student.ncirl.ie>
2020-10-20 14:35:35 +0000

    correct relative path references

--- mongotest.sh
+++ mongotest.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #Run workload A load and run
-./bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $2 $3}' > MongoDBoutLoadA.txt
+datadir="../../ycsb-0.17.0"
+
+$datadir/bin/ycsb load mongodb -s -P $datadir/workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $2 $3}' > MongoDBoutLoadA.txt
 #sleep 10
-./bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $2 $3}'  > MongoDBoutRunA.txt
+$datadir/bin/ycsb run mongodb -s -P $datadir/workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $2 $3}'  > MongoDBoutRunA.txt
 #sleep 10
-./bin/ycsb load mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadB.txt
+$datadir/bin/ycsb load mongodb -s -P $datadir/workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadB.txt
 #sleep 10
-./bin/ycsb run mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunB.txt
+$datadir/bin/ycsb run mongodb -s -P $datadir/workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunB.txt
 #sleep 10
-./bin/ycsb load mongodb -s -P workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadC.txt
+$datadir/bin/ycsb load mongodb -s -P $datadir/workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadC.txt
 #sleep 10
-./bin/ycsb run mongodb -s -P workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunC.txt
+$datadir/bin/ycsb run mongodb -s -P $datadir/workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunC.txt
 #sleep 10
-./bin/ycsb load mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadD.txt
+$datadir/bin/ycsb load mongodb -s -P $datadir/workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutLoadD.txt
 #sleep 10
-./bin/ycsb run mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunD.txt
+$datadir/bin/ycsb run mongodb -s -P $datadir/workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | awk '{print $3}' > MongoDBoutRunD.txt
 
 
 # merge the results file into one csv
--- redistest.sh
+++ redistest.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $2 $3}' > REDISoutLoadA.txt
+datadir="../../ycsb-0.17.0"
+
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $2 $3}' > REDISoutLoadA.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $2 $3}' > REDISoutRunA.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $2 $3}' > REDISoutRunA.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadB.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadB.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunB.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunB.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadC.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadC.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunC.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunC.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadD.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutLoadD.txt
 sleep 5
-./bin/ycsb load redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunD.txt
+$datadir/bin/ycsb load redis -s -P $datadir/workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" | awk '{print $3}' > REDISoutRunD.txt
 
 #merge results into csv
 perl loadmerge.pl REDISoutLoad* > redis_load_results.csv
