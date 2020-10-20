071a64646f1fc6127c15b0dfe475e74908eb9869
ceebish <72464213+x20157576@users.noreply.github.com>
2020-10-20 20:21:20 +0100

    Update mongotest.sh
    removed the comment on the sleep 10 and changed it to sleep 5.

--- mongotest.sh
+++ mongotest.sh
@@ -3,19 +3,19 @@
 datadir="../../ycsb-0.17.0"
 
 $datadir/bin/ycsb load mongodb -s -P  workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $2 $3}' > MongoDBoutLoadA.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb run mongodb -s -P  workloads/workloada -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $2 $3}'  > MongoDBoutRunA.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb load mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutLoadB.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb run mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutRunB.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb load mongodb -s -P workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutLoadC.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb run mongodb -s -P workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutRunC.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb load mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutLoadD.txt
-#sleep 10
+sleep 5
 $datadir/bin/ycsb run mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutRunD.txt
 
 # merge the results file into one csv
