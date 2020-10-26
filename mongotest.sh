ec8bcca9b5846c6d2eba560f3759a8783ef652d8
x20157576 <x20157576@student.ncirl.ie>
2020-10-26 16:09:41 +0000

    more edits

--- mongotest.sh
+++ mongotest.sh
@@ -15,8 +15,6 @@ sleep 5
 $datadir/bin/ycsb run mongodb -s -P workloads/workloadc -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutRunC.txt
 sleep 5
 $datadir/bin/ycsb load mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutLoadD.txt
-sleep 5
-$datadir/bin/ycsb run mongodb -s -P workloads/workloadd -p mongodb.url=mongodb://localhost:27017/ycsb?w=0 | grep -v "mongo client" | awk '{print $3}' > MongoDBoutRunD.txt
 
 # merge the results file into one csv
 perl loadmerge.pl MongoDBoutLoad* > mongodb_load_results.csv
